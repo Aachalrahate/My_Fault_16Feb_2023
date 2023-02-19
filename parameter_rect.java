3a7881492bb5c5ff1db087fb46ee9ba95c5bac7b
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-20 00:22:04 +0530

    parameter rectangle

---
+++ parameter_rect.java
@@ -0,0 +1,13 @@
+class parameter_rect
+{
+public static void main(String[] args)
+{
+int area;
+int parameter;
+int h=7,w=5;
+parameter=2*(h+w);
+System.out.println("parameter og rectangle="+parameter);
+area=w*h;
+System.out.println("area of rectangle="+area);
+}
+}
