cef14dc2d45295674454d141cb2c1076ecbec0c8
Aachalrahate <118259044+Aachalrahate@users.noreply.github.com>
2023-02-20 00:16:03 +0530

    addition of 5 no

---
+++ add_of_5no.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class add_of_5no
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the 5 no");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+int d=sc.nextInt();
+int e=sc.nextInt();
+int sum=a+b+c+d+e;
+System.out.println("addition of 5 no="+sum);
+}
+}
